76650a9ceba01de93ed16d5d686cc4ec71847bd3
safwanvk <safwanvalakundil@gmail.com>
2020-08-16 14:49:13 +0530

    :lipstic: Add templae attendence_details

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'student'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('attendence/', views.view_attendence, name='view-attendence')

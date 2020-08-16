778d151edb18d247cc2f556827b5d8d9c06b6023
safwanvk <safwanvalakundil@gmail.com>
2020-08-16 20:25:27 +0530

    :lipstick: Add template register

--- erp/urls.py
+++ erp/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('student.urls'))
+    path('', include('student.urls')),
+    path('', include('accounts.urls'))
 ]

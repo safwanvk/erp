ba9edda415fb1983aac062e7f54271202b62b0fe
safwanvk <safwanvalakundil@gmail.com>
2020-08-16 10:27:19 +0530

    :lipstic: Add templates base, index

--- erp/settings.py
+++ erp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'erp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'teplates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
